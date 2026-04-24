0fe37e2988f88bda5cbe970baf75e7eb4b615cc1
Martin Bažík <martin@bazo.sk>
2026-04-24 15:54:15 +0200

    add tanstack query and tsr

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { ThemeProvider } from "./components/theme-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./style.css";
 
 // Register things for typesafety
@@ -18,10 +19,14 @@ const router = createRouter({
 	scrollRestoration: true,
 });
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<ThemeProvider defaultTheme="system">
-			<RouterProvider router={router} />
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+			</QueryClientProvider>
 		</ThemeProvider>
 	</StrictMode>,
 );
